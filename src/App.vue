f981e928c9e6af86445ed673b5ab669e01691b6e
Nicooo <saoutn@gmail.com>
2021-10-26 10:14:20 +0200

    refactor(header) Move header (app-bar) in AppHeader.vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="white" flat>
-      <v-toolbar-title>AR-RICE</v-toolbar-title>
-
-      <v-spacer />
-
-      <v-btn plain text small href="#"> Home </v-btn>
-      <v-btn plain text small href="#"> About </v-btn>
-      <v-btn plain text small href="#"> Customer </v-btn>
-      <v-btn plain text small href="#"> Contact Us </v-btn>
-
-      <v-spacer />
-
-      <v-btn plain icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn plain icon>
-        <v-icon>mdi-cart-outline</v-icon>
-      </v-btn>
-
-      <v-btn plain icon>
-        <v-icon>mdi-account-outline</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <app-header />
 
     <v-main>
       <v-container> Hello World </v-container>
@@ -32,9 +9,13 @@
 </template>
 
 <script>
+import AppHeader from '@/components/AppHeader';
+
 export default {
   name: 'App',
-  components: {}
+  components: {
+    AppHeader
+  }
 };
 </script>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,28 @@
 <template>
-  <div></div>
+  <v-app-bar app color="white" flat>
+    <v-toolbar-title>AR-RICE</v-toolbar-title>
+
+    <v-spacer />
+
+    <v-btn plain text small href="#"> Home </v-btn>
+    <v-btn plain text small href="#"> About </v-btn>
+    <v-btn plain text small href="#"> Customer </v-btn>
+    <v-btn plain text small href="#"> Contact Us </v-btn>
+
+    <v-spacer />
+
+    <v-btn plain icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-btn plain icon>
+      <v-icon>mdi-cart-outline</v-icon>
+    </v-btn>
+
+    <v-btn plain icon>
+      <v-icon>mdi-account-outline</v-icon>
+    </v-btn>
+  </v-app-bar>
 </template>
 
 <script>
