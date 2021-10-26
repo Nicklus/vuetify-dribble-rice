2c1635a3e854520c677066e64b73c235a596a30c
Nicooo <saoutn@gmail.com>
2021-10-26 10:11:24 +0200

    feat(App.vue) Add header (app-bar)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
   <v-app>
-    <v-alert border="top" color="red lighten-2" dark>
-      I'm an alert with a top border and red color
-    </v-alert>
+    <v-app-bar app color="white" flat>
+      <v-toolbar-title>AR-RICE</v-toolbar-title>
+
+      <v-spacer />
+
+      <v-btn plain text small href="#"> Home </v-btn>
+      <v-btn plain text small href="#"> About </v-btn>
+      <v-btn plain text small href="#"> Customer </v-btn>
+      <v-btn plain text small href="#"> Contact Us </v-btn>
+
+      <v-spacer />
+
+      <v-btn plain icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn plain icon>
+        <v-icon>mdi-cart-outline</v-icon>
+      </v-btn>
+
+      <v-btn plain icon>
+        <v-icon>mdi-account-outline</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container> Hello World </v-container>
+    </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: "App",
-  components: {},
+  name: 'App',
+  components: {}
 };
 </script>
 
---
+++ src/components/AppMain.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-alert border="top" color="red lighten-2" dark>
+    I'm an alert with a top border and red color
+  </v-alert>
+</template>
+
+<script>
+export default {
+  name: 'AppMain'
+};
+</script>
+
+<style></style>
